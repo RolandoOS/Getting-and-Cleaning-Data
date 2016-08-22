1ae777e8d500929964e3b2ca0db5475b511d70dd
Rolando Olivas <RolandoOS@users.noreply.github.com>
2016-08-21 22:02:20 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -2,13 +2,15 @@ print("Getting and Cleaning Data - Project")
 
 ptm = proc.time()
 
+print("Loading libraries: dplyr, tidyr...")
+
 library(dplyr)
 library(tidyr)
 # library(data.table)
 
 # 0. Download and extract data.
 
-print("Downloading Files")
+print("Downloading Files...")
 
 #setwd("/Users/developer3/Desktop/JHU_DS_Cert/3_Getting_Data/Project/")
 
@@ -22,7 +24,7 @@ unzip("Dataset.zip")
 
 # 1. Merges the training and the test sets to create one data set. ----
 
-print("Extracting Data")
+print("Extracting Data...")
 
 features <- tbl_df(read.csv("./UCI HAR Dataset/features.txt", header = FALSE, stringsAsFactors = FALSE, sep=""))
 
@@ -41,15 +43,25 @@ aux_train <- mutate(X_train, subject=subject_train$subjects, activity=y_train$ac
 aux_test <- mutate(X_test, subject=subject_test$subjects, activity=y_test$activities, run = 'test') %>%
   select(subject, activity, run, starts_with("V"))
 
+print("Merging the training and testing data sets.")
+
 Data_0 <- bind_rows(aux_test, aux_train)
+
+print("Naming variables with feature names.")
 
 names(Data_0)[4:564] <- features$V2 #as.list()
 
+print("Cleaning work space.")
+
 rm(list=setdiff(ls(), c("ptm", "Data_0")))
+
+print("Removing duplicate variables.")
 
 Data_1 <- Data_0[ , !duplicated(colnames(Data_0))]
 
 # 2. Extracts only the measurements on the mean and standard deviation for each measurement. ----
+
+print("Extracting mean and standard deviation of measurements.")
 
 Data_1 <- Data_1 %>% select(subject, activity, run, contains("mean()") , contains("std()"))
 
@@ -62,6 +74,8 @@ Data_1 <- Data_1 %>% select(subject, activity, run, contains("mean()") , contain
 # 5 STANDING
 # 6 LAYING
 
+print("Assigning descriptive activities names.")
+
 Data_1 <- Data_1 %>% mutate(activity = replace(activity, activity==1, "WALKING")) %>%
   mutate(activity = replace(activity, activity==2, "WALKING_UPSTAIRS")) %>%
   mutate(activity = replace(activity, activity==3, "WALKING_DOWNSTAIRS")) %>%
@@ -70,6 +84,8 @@ Data_1 <- Data_1 %>% mutate(activity = replace(activity, activity==1, "WALKING")
   mutate(activity = replace(activity, activity==6, "LAYING"))
 
 # 4. Appropriately labels the data set with descriptive variable names. ----
+
+print("Assigning descriptive feature names.")
 
 names(Data_1) <- gsub("Acc", "Accelerometer", names(Data_1))
 names(Data_1) <- gsub("Mag", "Magnitude", names(Data_1))
@@ -80,12 +96,17 @@ names(Data_1) <- gsub("^f", "Frequency-", names(Data_1))
 names(Data_1) <- gsub("mean\\(\\)", "MEAN", names(Data_1))
 names(Data_1) <- gsub("std\\(\\)", "SD", names(Data_1))
 
+print("Writing tidy Dataset Output: Tidy_Data_1.txt")
+write.table(Data_1, file = "./Tidy_Data.txt", row.names = FALSE)
+
 # 5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject. ----
 
 Data_2 <- Data_1 %>% group_by(subject, activity) %>% select(subject, activity, contains("MEAN"))  %>%
   summarise_each(funs(mean(., na.rm = TRUE)))
 
-write.table(Data_2, file = "./Data_Summary.txt", row.names = FALSE)
+print("Writing summary  tidy Dataset Output: Tidy_Data_1.txt")
+
+write.table(Data_2, file = "./Tidy_Data_summary.txt", row.names = FALSE)
 
 ftm = proc.time() - ptm
 
